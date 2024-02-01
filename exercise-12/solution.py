b6584cb4695e9efd924d549a9ee2bd981be1f18d
Jared-Arvind <7701698+inveniam-viam@users.noreply.github.com>
2024-01-31 19:21:14 -0500

    adding solution to exercise 12 main problem

---
+++ exercise-12/solution.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+def most_repeating_word(input_seq: list|tuple) -> str:
+
+    # creating a list to store the number of times the most frequen
+
+    letter_repeat_cts: list = [sorted(Counter(item).most_common(1))[0][1]for item in input_seq]
+
+    return input_seq[letter_repeat_cts.index(max(letter_repeat_cts))]
+
+if __name__ == "__main__":
+
+    words = ['this', 'is', 'an', 'elementary', 'test', 'example']
+
+    print(most_repeating_word(words))
