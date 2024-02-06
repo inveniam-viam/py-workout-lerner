cb00d4099d5b31870d3001e0a6ae516abb852a8a
Jared-Arvind <7701698+inveniam-viam@users.noreply.github.com>
2024-02-06 17:58:48 -0500

    code and source file for main problem from exercise 20

---
+++ exercise-20/solution.py
@@ -0,0 +1,30 @@
+# Solution to main problem from exercise 20
+
+def implement_wc(in_txt_file: str) -> dict:
+
+    out_dict: dict = dict.fromkeys(("lines", "characters", "words", "unique words"), 0)                    # going to parse the file into a dictionary
+
+    unique_words: set = set()
+
+    with open(in_txt_file, "r", encoding = "utf-8") as f:
+
+        for line in f:
+
+            out_dict["lines"] += 1
+
+            out_dict["characters"] += len(line)
+
+            out_dict["words"] += len(line.split())
+
+            unique_words.update(line.split())
+        
+    out_dict["unique words"] += len(unique_words)
+
+    return out_dict 
+
+
+out_info = implement_wc("./sample.txt")
+
+for key, val in out_info.items():
+
+    print(f"The number of {key} in the file is equal to {val}")
